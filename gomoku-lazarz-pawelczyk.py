c35bc19f76a97ee52b4a14b8aa1ad75c3c83aa74
Sajmon444 <113638328+Sajmon444@users.noreply.github.com>
2023-05-03 18:12:50 +0200

    zrobione wstawianie pozycji graczy do planszy

--- gomoku-lazarz-pawelczyk.py
+++ gomoku-lazarz-pawelczyk.py
@@ -2,7 +2,7 @@ import os
 from colorama import Fore, Back
 
 def mapa(tablica_graczy):
-    os.system('cls')
+    #os.system('cls')
 
     narozniki={
         "znak1": "┌",
@@ -33,8 +33,8 @@ def mapa(tablica_graczy):
         for j in wiersz:
             if j=="X":
                 print(Fore.GREEN," X ",end="",sep="")
-            elif j=="Y":
-                print(Fore.RED," Y ",end="",sep="")
+            elif j=="O":
+                print(Fore.RED," O ",end="",sep="")
             else:
                 print(Fore.WHITE,"   ",end="",sep="")
             print(Fore.WHITE,linie["pion"],end="",sep="")
@@ -54,3 +54,23 @@ if __name__ == "__main__":
         kolumna = [0 for i in range(rozmiar)]
         gracze.append(kolumna)
     mapa(gracze)
+
+
+    gracz = "X"
+    while True:
+        mapa(gracze)
+        if gracz == "X": print(Fore.GREEN,"Gracz 1\n")
+        else: print(Fore.RED,"Gracz 2\n")
+
+        sprawdzanie=True
+        while sprawdzanie==True :    
+            x = int(input("Podaj wsp x: "))
+            y = int(input("Podaj wsp y: "))
+            if gracze[y][x]!="X" and  gracze[y][x]!="O":
+                
+                sprawdzanie=False
+        gracze[y][x] = gracz
+        if gracz =="X":
+            gracz="O"
+        elif gracz=="O":
+            gracz="X"
